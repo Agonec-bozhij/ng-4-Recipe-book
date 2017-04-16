53a5e4e87a356f9dc8674da65bb1ce021aeee69f
Agutins-infernal-snares <dmitriy321990@mail.ru>
2017-04-16 12:27:32 +0300

    Initial

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,48 @@
 import {Recipe} from "./recipe.model";
-import {EventEmitter} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
+
+@Injectable()
+
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe1', 'This is simply a test1', 'http://www.ipa.co.uk/write/imageportfolio/recipe_logo.jpg'),
-        new Recipe('A Test Recipe2', 'This is simply a test2', 'http://www.ipa.co.uk/write/imageportfolio/recipe_logo.jpg')
+        new Recipe(
+            'Сырный суп по‑французски, с курицей',
+            'Очень захотелось сварить вкусный куриный супчик на обед. Порекомендую нежный сырный суп с курицей.',
+            'http://img1.russianfood.com/dycontent/images_upl/91/big_90812.jpg',
+            [
+                new Ingredient('Филе куриное', 500),
+                new Ingredient('Сыр плавленый', 200),
+                new Ingredient('Картофель', 400),
+                new Ingredient('Лук репчатый', 150)
+            ]
+        ),
+        new Recipe(
+            'Итальянская паста «Карбонара» с беконом и сыром',
+            'Подсолить воду, довести до кипения и отварить спагетти почти до готовности, время варки указано на упаковке. Откинуть на дуршлаг и вернуть в сухую кастрюлю. Отдельно измельчить бекон, порубить очищенный репчатый лук и чеснок. Все ингредиенты обжарить на сковороде до золотистого цвета в течение 5 – 6 минут.',
+            'http://www.prigotovit.info/uploads/posts/2017-03/1489771152_15italyanskaya-pasta-karbonara-s-bekonom-i-syrom.jpg',
+            [
+                new Ingredient('Спагетти', 150),
+                new Ingredient('Сливки', 150),
+                new Ingredient('Бекон', 100),
+                new Ingredient('Сыр твердый', 20)
+            ]
+        ),
     ];
+
+    constructor(private slService: ShoppingListService) {
+
+    }
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
